0824965f3261c2602ebb106cfdce8f95f2c944a2
Bruno_local <brunocesar.br@gmail.com>
2025-09-04 13:02:36 -0300

    Atualizado o app suppliers
    - Adicionado verbose_name = 'Fornecedores' em models.py;
    - Registrado o app no painel do admin;
    - Adicionado verbose_name = 'Cadastrar Fornecedores' em apps.py.

--- suppliers/models.py
+++ suppliers/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Supplier(models.Model):
-    name = models.CharField(max_length=500, verbose_name='Nome')
+    name = models.CharField(max_length=500, verbose_name='Fornecedor')
     description = models.TextField(null=True, blank=True, verbose_name='Descrição')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de criação')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Última atualização')
 
     class Meta:
+        verbose_name = 'Fornecedor'
         ordering = ['name']
 
     def __str__(self):
