08bb2e99af90fbad7639e511d78906f280476f13
Bruno_local <brunocesar.br@gmail.com>
2025-09-04 10:57:18 -0300

    Criado as tabelas (models) de inflows
    - python manage.py makemigrations
    - Adicionalmente ao mostrado no curso foi adicionado o parâmetro "verbose_name" a cada campo de tabela.

--- inflows/models.py
+++ inflows/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from suppliers.models import Supplier
+from products.models import Product
 
-# Create your models here.
+
+class Inflow(models.Model):
+    supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT, related_name='inflows', verbose_name='Fornecedor')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='inflows', verbose_name='Produto')
+    quantity = models.IntegerField(default=0, verbose_name='Quantidade')
+    description = models.TextField(null=True, blank=True, verbose_name='Descrição')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de criação')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Última Atualização')
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return str(self.product)
