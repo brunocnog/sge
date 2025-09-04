18b203cfef5fb99e01b8d5bb2e9c19058f0b665f
Bruno_local <brunocesar.br@gmail.com>
2025-09-04 10:23:44 -0300

    Criado as tabelas (models) de suppliers
    - python manage.py makemigrations
    - Adicionalmente ao mostrado no curso foi adicionado o parâmetro "verbose_name" a cada campo de tabela.

--- suppliers/models.py
+++ suppliers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=500, verbose_name='Nome')
+    description = models.TextField(null=True, blank=True, verbose_name='Descrição')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de criação')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Última atualização')
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
