ced4f5c5d62a0e9c490177c81a8d903e038d8f4d
Bruno_local <brunocesar.br@gmail.com>
2025-09-04 12:57:48 -0300

    Atualizado o app categories
     - Adicionado verbose_name = 'Categorias' em models.py;
     - Registrado o app no painel do admin;
     - Adicionado verbose_name = 'Cadastrar Categorias' em apps.py.

--- categories/models.py
+++ categories/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=500, verbose_name='Nome')
+    name = models.CharField(max_length=500, verbose_name='Categoria')
     description = models.TextField(null=True, blank=True, verbose_name='Descrição')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de criação')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Última atualização')
 
     class Meta:
+        verbose_name = 'Categoria'
         ordering = ['name']
 
     def __str__(self):
