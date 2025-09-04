e629ec121b7c592970b19aedaf9bb256abcc4ef9
Bruno_local <brunocesar.br@gmail.com>
2025-09-04 12:46:26 -0300

    Atualizado o app brands
     - Adicionado verbose_name = 'Marca' em models.py;
     - Registrado o app no painel do admin;
     - Adicionado verbose_name = 'Cadastrar Marcas' em apps.py.
     - py manage.py makemigrations
     - py manage.py migrate

--- brands/admin.py
+++ brands/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Brand
 
-# Register your models here.
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'created_at', 'updated_at']
+    search_fields = ['name']
--- brands/models.py
+++ brands/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length=500, verbose_name='Nome')
+    name = models.CharField(max_length=500, verbose_name='Marca')
     description = models.TextField(null=True, blank=True, verbose_name='Descrição')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de criação')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Última atualização')
 
     class Meta:
+        verbose_name = 'Marca'
         ordering = ['name']
 
     def __str__(self):
