ab239ee8983a5ae50e5a4a12d7dfe9b01cf74952
Bruno_local <brunocesar.br@gmail.com>
2025-09-05 15:08:02 -0300

    Criado o forms.py e brand_create.html

--- brands/urls.py
+++ brands/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BrandListView
+from .views import BrandListView, BrandCreateView
 
 
 urlpatterns = [
     path('brands/list/', BrandListView.as_view(), name='brand_list'),
+    path('brands/create/', BrandCreateView.as_view(), name='brand_create')
 ]
--- brands/views.py
+++ brands/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.db.models.query import QuerySet
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 from .models import Brand
+from .forms import BrandForm
 
 
 class BrandListView(ListView):
@@ -17,4 +17,11 @@ class BrandListView(ListView):
             queryset = queryset.filter(name__icontains=name)
 
         return queryset
+    
+
+class BrandCreateView(CreateView):
+    model = Brand
+    template_name = 'brand_create.html'
+    form_class = BrandForm
+    success_url = reverse_lazy('brand_list')
     
