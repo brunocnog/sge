14a79eea37b209d0bdc06fe969849458673d93fe
Bruno_local <brunocesar.br@gmail.com>
2025-09-09 15:52:30 -0300

    Criado o CRUD de Entradas

--- app/urls.py
+++ app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', include('categories.urls')),
     path('', include('suppliers.urls')),
     path('', include('products.urls')),
-    # path('', include('inflows.urls')),
+    path('', include('inflows.urls')),
     # path('', include('outflows.urls')),
 ]
