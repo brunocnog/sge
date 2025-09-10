8983d12c54c07372d83035ac35a168ea415826ca
Bruno_local <brunocesar.br@gmail.com>
2025-09-10 14:29:58 -0300

    Criado a estrutura inicial de dashboard

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', home, name='home'),
 
     path('', include('brands.urls')),
     path('', include('categories.urls')),
