b17ff2b56effd49ef6be83286bf29e99bb35b3a8
Bruno_local <brunocesar.br@gmail.com>
2025-09-11 14:41:45 -0300

    Complementando o gráfico de vendas (vendas diárias)

--- app/views.py
+++ app/views.py
@@ -1,16 +1,19 @@
 import json
 from django.shortcuts import render
-from .metrics import get_product_metrics, get_sales_metrics, get_daily_sales_data
+# from .metrics import get_product_metrics, get_sales_metrics, get_daily_sales_data, get_daily_sales_quantity_data
+from . import metrics
 
 def home(request):
-    product_metrics = get_product_metrics()
-    sales_metrics = get_sales_metrics()
-    daily_sales_data = get_daily_sales_data()
+    product_metrics = metrics.get_product_metrics()
+    sales_metrics = metrics.get_sales_metrics()
+    daily_sales_data = metrics.get_daily_sales_data()
+    daily_sales_quantity_data = metrics.get_daily_sales_quantity_data()
 
     context = {
         'product_metrics': product_metrics,
         'sales_metrics': sales_metrics,
         'daily_sales_data': json.dumps(daily_sales_data),
+        'daily_sales_quantity_data':json.dumps(daily_sales_quantity_data),
     }
 
     return render(request, 'home.html', context)
