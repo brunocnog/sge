f86dfb05a5677adfea3e317ed29d2c6b9a459a1c
Bruno_local <brunocesar.br@gmail.com>
2025-09-11 14:28:26 -0300

    Complementando o gráfico de vendas dos últimos 7 dias

--- app/views.py
+++ app/views.py
@@ -1,13 +1,16 @@
+import json
 from django.shortcuts import render
-from .metrics import get_product_metrics, get_sales_metrics
+from .metrics import get_product_metrics, get_sales_metrics, get_daily_sales_data
 
 def home(request):
     product_metrics = get_product_metrics()
     sales_metrics = get_sales_metrics()
+    daily_sales_data = get_daily_sales_data()
 
     context = {
         'product_metrics': product_metrics,
-        'sales_metrics': sales_metrics
+        'sales_metrics': sales_metrics,
+        'daily_sales_data': json.dumps(daily_sales_data),
     }
 
     return render(request, 'home.html', context)
