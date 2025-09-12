6573b4c90fa0746a9213bd8343033eb751e5a92c
Bruno_local <brunocesar.br@gmail.com>
2025-09-11 22:17:12 -0300

    Criado rota de login

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 
     path('', home, name='home'),
 
