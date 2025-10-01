eddde278817037e410237859791cee913a0de8d2
Bruno_local <brunocesar.br@gmail.com>
2025-10-01 14:29:05 -0300

    Criado a app authentication

--- app/settings.py
+++ app/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'products',
     'inflows',
     'outflows',
+    'authentication',
 
 ]
 
@@ -145,6 +146,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.DjangoModelPermissions',
     ),
 }
 
