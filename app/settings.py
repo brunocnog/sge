531eb5ff555e84eacf8cf72e54333e3d6a8e4e1a
Bruno_local <brunocesar.br@gmail.com>
2025-10-03 00:04:54 -0300

    Projeto Dockerizado

--- app/settings.py
+++ app/settings.py
@@ -92,7 +92,16 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sge',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'sge_db',
+        'PORT': '5432',
+    },
+
+    'dev': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
