1931f412b63377ec7b1363515df4bfc5a5612775
Bruno_local <brunocesar.br@gmail.com>
2025-10-23 13:47:23 -0300

    Atualização Webhook

--- services/notify.py
+++ services/notify.py
@@ -1,13 +1,19 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 class Notify:
 
     def __init__(self):
         self.__base_url = 'https://webhook.site'
+        self.__api_key = os.getenv('WEBHOOK_API_KEY')
 
-    def send_event(self, data):
+    def send_order_event(self, data):
         requests.post(
-            url=f'{self.__base_url}/e45162e6-2852-41fc-93b9-fee461f6c6d7',
+            url=f'{self.__base_url}/{self.__api_key}',
             json=data,
         )
